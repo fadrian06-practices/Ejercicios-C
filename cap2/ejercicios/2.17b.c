eccc11a2de68c9788743a43a9e166acbcf3467c5
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-18 18:40:26 -0400

    Separado el ejercicio 2.17 en 3 secciones a, b y c

---
+++ cap2/ejercicios/2.17b.c
@@ -0,0 +1,17 @@
+/*
+  Escriba un programa que imprima los números del 1 al 4 en la misma línea.
+  Escriba el programa utilizando los siguientes métodos:
+
+  a) Mediante una instrucción printf sin especificadores de conversión.
+  b) Mediante una instrucción printf con cuatro especificadores de conversión.
+  c) Mediante cuatro instrucciones printf.
+ */
+
+/* Imprime los números del 1 al 4 */
+#include <stdio.h>
+
+int main() {
+  printf("%d %d %d %d\n", 1, 2, 3, 4);
+
+  return 0;
+}
